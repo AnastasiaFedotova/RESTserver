48e704699d00a34da4e5fd593a395f94cb6415d1
Nastassia Fiadotava <nastassia_fiadotava@epam.com>
2021-05-16 13:57:56 +0300

    feat: add custom database

---
+++ src/common/customdb.js
@@ -0,0 +1,48 @@
+const Tables = [];
+
+class Table {
+    constructor(name) {
+        this.name = name;
+        this.data = [];
+    }
+
+    async getItems() {
+        return this.data;
+    }
+
+    async getItem(itemId) {
+        const items = await this.getItems();
+        const item = items.find((elem) => elem.id === itemId);
+
+        return item;
+    }
+
+    async addItem (item) {
+        this.data.push(item);
+
+        return item;
+    }
+
+    async updateItem(itemId, newItem) {
+        const items = await this.getItems();
+        const index = items.findIndex((item) => item.id === itemId);
+        items[index] = newItem;
+
+        return newItem;
+    }
+
+    async removeItem(itemId) {
+        const items = await this.getItems();
+        const index = items.findIndex((item) => item.id === itemId);
+        const coontRemove = 1;
+
+        return items.splice(index, coontRemove);
+    }
+}
+
+const createTable = (name) => {
+    const indexTable = Tables.push(new Table(name)) - 1;
+    return Tables[indexTable];
+}
+
+module.exports = createTable;
