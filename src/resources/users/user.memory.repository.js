dc7896dade5f8e96c1cf8edeb35679a8fd7724bc
Nastassia Fiadotava <nastassia_fiadotava@epam.com>
2021-05-16 15:41:38 +0300

    feat: add users table

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,34 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const createTable = require("../../common/customdb");
+
+const usersTable = createTable("users");
+
+const getAllUsers = () => usersTable.getItems();
+
+const getUser = (usersId) => {
+  const user = usersTable.getItem(usersId);
+  return user;
 };
 
-module.exports = { getAll };
+const addUser = (user) => {
+  const newUser = usersTable.addItem(user);
+  return newUser;
+};
+
+const updateUser = (usersId, newUsers) => {
+  const user = usersTable.updateItem(usersId, newUsers);
+  return user;
+};
+
+const removeUser = (usersId) => {
+  const user = usersTable.removeItem(usersId);
+  return user;
+};
+
+module.exports = { 
+  getAllUsers,
+  getUser,
+  addUser,
+  updateUser,
+  removeUser
+};
+
