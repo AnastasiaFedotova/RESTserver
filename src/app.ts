1ebe2448eff2bc3745e894e66a5720e7f8d14ad8
Anastasia <anastaisafedotova@gmail.com>
2021-06-06 22:07:12 +0200

    refactor: include logging

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,14 @@ import path from 'path';
 import yaml from 'yamljs';
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
+import rrTracer from './middlewares/rrTracer';
+import unhandledExceptions from './middlewares/unhandledExceptions'
 
 const app = express();
 const swaggerDocument = yaml.load(path.join(__dirname, '../doc/api.yaml'));
+
+app.use(unhandledExceptions);
+app.use(rrTracer);
 
 app.use(express.json());
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
@@ -21,5 +26,6 @@ app.use('/', (req, res, next) => {
 
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
+
 
 export { app }
