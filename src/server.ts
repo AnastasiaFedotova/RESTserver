a364ddb1d2e5323f91ff47833fcde00b3e6876f6
Anastasia <anastaisafedotova@gmail.com>
2021-06-06 22:19:07 +0200

    feat: add log listener

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
 const { PORT } = require('./common/config');
 import { app } from './app';
+import logger from './common/logger'
 
 app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
+  logger.logInfo(`App is running on http://localhost:${PORT}`)
 );
+
+process.on('unhandledRejection', (reason, p) => {
+    logger.logWarn('Unhandled Rejection at Promise', reason, p);
+  })
+  .on('uncaughtException', err => {
+    logger.logError('Uncaught Exception thrown', err);
+    process.exit(1);
+  });
